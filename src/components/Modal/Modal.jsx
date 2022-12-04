287912795aab72bd4ce71af75b679e0bd37c77c7
Екатерина <Katderl@ukr.net>
2022-12-04 10:15:23 +0200

    summary changed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
-import { BsXLg } from 'react-icons/bs';
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
@@ -35,18 +34,13 @@ export class Modal extends Component {
   };
 
   render() {
-    const { title, onClose, currentImageUrl, currentImageDescription } =
+    const { currentImageUrl, currentImageDescription } =
       this.props;
 
     return createPortal(
       <div className={css.backdrop} onClick={this.handleClickBackdrop}>
         <div className={css.modal}>
-          <div className={css.wrapper}>
-            {title && <h1 className={css.title}>{title}</h1>}
-            <button className={css.button} type="button" onClick={onClose}>
-              <BsXLg className={css.icon} />
-            </button>
-          </div>
+          
           <img
             src={currentImageUrl}
             alt={currentImageDescription}
